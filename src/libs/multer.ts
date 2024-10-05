14015bbd996a10bdf157878fc1e9dcebe7f8a6f8
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-10-05 13:09:36 -0600

    Update multer.ts

--- src/libs/multer.ts
+++ src/libs/multer.ts
@@ -1,12 +1,12 @@
-import multer from 'multer'
-import path from 'path'
-import uuid from 'uuid/v4';
+import multer from 'multer';
+import path from 'path';
+import { v4 as uuid } from 'uuid';
 
 // Settings
 const storage = multer.diskStorage({
     destination: 'uploads',
     filename: (req, file, cb) => {
-        cb(null, uuid() + path.extname(file.originalname))
+        cb(null, uuid() + path.extname(file.originalname));
     }
 });
-export default multer({storage});
+export default multer({ storage });
