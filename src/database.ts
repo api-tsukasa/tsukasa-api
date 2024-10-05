743cdd4bb1e7b80296cb25d29c6eea9ee093deb3
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-10-05 13:09:09 -0600

    Update database.ts

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,6 @@
-import { connect } from 'mongoose'
+import { connect } from 'mongoose';
 
 export async function startConnection() {
-    const db = await connect('mongodb://localhost/tsukaApiGalery',{
-        useNewUrlParser: true,
-        useFindAndModify: false 
-    });
+    const db = await connect('mongodb://localhost/tsukaApiGalery');
     console.log('Database is connected');
 }
